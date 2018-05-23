4bcfe9ffe8268d85e14e6cb8ba87640629409256
Edgars <edgars.salmins@gmail.com>
2018-05-23 15:25:51 +0300

    assembly info edited.

--- log4net.Azure/Properties/AssemblyInfo.cs
+++ log4net.Azure/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("log4net.Azure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Geta.log4net.Azure")]
+[assembly: AssemblyDescription("log4net azure appender")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Karell Ste-Marie")]
-[assembly: AssemblyProduct("log4net.Azure")]
-[assembly: AssemblyCopyright("Copyright ©  2012-2015 Karell Ste-Marie")]
-[assembly: AssemblyTrademark("All Rights Reserved")]
+[assembly: AssemblyCompany("Geta")]
+[assembly: AssemblyProduct("Geta.log4net.Azure")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("02fbff4a-2f04-4eed-8537-997924c7fa10")]
+[assembly: Guid("DFED3AFF-ECAB-479B-B1DA-E084DD7471BC")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.*")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
